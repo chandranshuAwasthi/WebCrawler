fa32796ef829377a7a18e58719032149abfacb4f
chandranshu <chandranshu.awasthi@bt.com>
2019-06-08 11:20:41 +0530

    WebCrawler project

---
+++ src/com/ge/crawler/dto/Pages.java
@@ -0,0 +1,18 @@
+package com.ge.crawler.dto;
+
+import java.util.List;
+
+public class Pages {
+
+	List<Page> pages;
+
+	public List<Page> getPages() {
+		return pages;
+	}
+
+	public void setPageList(List<Page> pages) {
+		this.pages = pages;
+	}
+	
+	
+}
